b0d97397477492627b5308af803bf8b8fd76cc0d
Ryan Dostalek <ryanedostalek@gmail.com>
2025-07-22 16:39:58 -0700

    Make GUI object visible after it is parented to the canvas group

--- Modal.lua
+++ Modal.lua
@@ -18,7 +18,7 @@ export type Modal = {
 	CloseButton: GuiButton?,
 	ToggleButton: GuiButton?,
 	InputObjects: {InputObject}?,
-	
+
 	SetButtons: (
 		self: Modal, 
 		openButton: GuiButton?, 
@@ -26,12 +26,12 @@ export type Modal = {
 		toggleButton: GuiButton?) 
 	-> Modal,
 	SetContext: (self: Modal, inputObjects: {InputObject}) -> Modal,
-	
+
 	Open: (self: Modal) -> (),
 	Close: (self: Modal) -> (),
 	Toggle: (self: Modal) -> (),
 	Destroy: (self: Modal) -> (),
-	
+
 	_canvasGroup: CanvasGroup,
 	_isAnimating: boolean,
 	_actionName: string,
@@ -46,32 +46,33 @@ local function wrapInCanvasGroup(guiObject: GuiObject) : CanvasGroup
 	canvasGroup.Size = UDim2.new(1, 0, 1, 0)
 	canvasGroup.Parent = guiObject.Parent
 	guiObject.Parent = canvasGroup
+	guiObject.Visible = true
 	return canvasGroup
 end
 
 -- Constructs a new modal object
 function Modal.New(guiObject: GuiObject, tweenInfo: TweenInfo?, blurSize: number?, isOpen: boolean?): Modal
 	local newModal: Modal = {}
-	
+
 	newModal.GuiObject = guiObject
 	newModal.TweenInfo = if tweenInfo ~= nil then tweenInfo else TweenInfo.new(0.15)
 	newModal.BlurSize = if blurSize ~= nil then blurSize else 24
 	newModal.IsOpen = if isOpen ~= nil then isOpen else false
-	
+
 	newModal._canvasGroup = wrapInCanvasGroup(guiObject)
 	newModal._actionName = tostring(newModal)
-	
+
 	local newBlurEffect = Instance.new("BlurEffect")
 	newBlurEffect.Size = if newModal.IsOpen then newModal.BlurSize else 0
 	newBlurEffect.Parent = Lighting
 	newModal._blurEffect = newBlurEffect
-	
+
 	-- Set initial state
 	newModal._isAnimating = false
 	newModal._canvasGroup.GroupTransparency = if newModal.IsOpen then 0 else 1
 	newModal._canvasGroup.Position = if newModal.IsOpen then OPEN_POSITION else CLOSED_POSITION
 	newModal._canvasGroup.Visible = newModal.IsOpen
-		
+
 	setmetatable(newModal, Modal)
 	return newModal
 end
@@ -93,71 +94,71 @@ function Modal:SetButtons(openButton: GuiButton?, closeButton: GuiButton?, toggl
 	self.OpenButton = openButton
 	self.CloseButton = closeButton
 	self.ToggleButton = toggleButton
-	
+
 	cleanUpConnections(self)
 	self._connections = {}
-	
+
 	if openButton then
 		local connection = openButton.Activated:Connect(function()
 			self:Open()
 		end)
 		table.insert(self._connections, connection)
 	end
-	
+
 	if closeButton then
 		local connection = closeButton.Activated:Connect(function()
 			self:Close()
 		end)
 		table.insert(self._connections, connection)
 	end
-	
+
 	if toggleButton then
 		local connection = toggleButton.Activated:Connect(function()
 			self:Toggle()
 		end)
 		table.insert(self._connections, connection)
 	end
-	
+
 	return self
 end
 
 -- Set context for toggling the modal
 function Modal:SetContext(inputObjects: {InputObject})
 	self.InputObjects = inputObjects
-	
+
 	local function handleToggle(_actionName, inputState: Enum.UserInputState, _inputObject)
 		if inputState == Enum.UserInputState.Begin then
 			self:Toggle()
 		end
 	end
-	
+
 	ContextActionService:BindAction(self._actionName, handleToggle, false, table.unpack(inputObjects))
-	
+
 	return self
 end
 
 -- Open the modal
 function Modal:Open()
 	if self.IsOpen or self._isAnimating then return end
-	
+
 	self.IsOpen = true
 	self._isAnimating = true
 	self._canvasGroup.Visible = true
-	
+
 	local openGoal = {
 		GroupTransparency = 0,
 		Position = OPEN_POSITION
 	}
 	local openTween = TweenService:Create(self._canvasGroup, self.TweenInfo, openGoal)
-	
+
 	local blurGoal = {
 		Size = self.BlurSize
 	}
 	local blurTween = TweenService:Create(self._blurEffect, self.TweenInfo, blurGoal)
-	
+
 	openTween:Play()
 	blurTween:Play()
-	
+
 	openTween.Completed:Once(function()
 		self._isAnimating = false		
 	end)
@@ -175,15 +176,15 @@ function Modal:Close()
 		Position = CLOSED_POSITION
 	}
 	local closeTween = TweenService:Create(self._canvasGroup, self.TweenInfo, closeGoal)
-	
+
 	local blurGoal = {
 		Size = 0
 	}
 	local blurTween = TweenService:Create(self._blurEffect, self.TweenInfo, blurGoal)
-	
+
 	closeTween:Play()
 	blurTween:Play()
-	
+
 	closeTween.Completed:Once(function()
 		self._canvasGroup.Visible = false
 		self._isAnimating = false
